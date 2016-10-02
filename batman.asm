bfed3b80ec7a118ca133c09e8db119734109c72e
luiz <luizfernandovieira_cf@hotmail.com>
2016-10-02 19:08:19 -0300

    azul funfando quase perfeitamente

--- batman.asm
+++ batman.asm
@@ -144,6 +144,7 @@
 	#		sqrt(1-(x/7)^2)+(5+0.97*(abs(x-0.5)+abs(x+0.5))-3 * (abs(x-0.75)+abs(x+0.75))) 
 	#		* 
 	#		(1+abs(1-abs(x))/(1-abs(x)))
+
 	FUNCAO_AZUL:
 		l.s $f20, ZERO
 		l.s $f21, TRES
@@ -156,59 +157,60 @@
 		l.s $f30, AZUL_AUX_2			# 0.5
 		l.s $f31, AZUL_AUX_3			# 0.75
 
-	  abs.s $f13, $f0        # f13 = abs (f0)		# abs(x)
+	  abs.s $f13, $f0        # f13 = abs (f0)			# abs(x)
 	  add.s $f17, $f20, $f21 # f17 = 3						# 3
-	  sub.s $f17, $f17, $f13 # f17 = f17 - f13 	# 3 - abs(x)
+	  sub.s $f17, $f17, $f13 # f17 = f17 - f13 		# 3 - abs(x)
 	  add.s $f16, $f20, $f17 # f16 = f17
 		abs.s $f17, $f17       # f17 = abs (f17)		# abs(3 - abs(x))
 	  sub.s $f13, $f13, $f22 # f13 = f13 - 1			# abs(x) - 1
 	  add.s $f15, $f20, $f13 # f15 = f13
-		abs.s $f13, $f13       # f13 = abs (f13) 	# abs(abs(x) - 1)
-		mul.s $f13, $f13, $f23 # f13 = -f13				# (-abs(abs(x) - 1))
+		abs.s $f13, $f13       # f13 = abs (f13) 		# abs(abs(x) - 1)
+		mul.s $f13, $f13, $f23 # f13 = -f13					# (-abs(abs(x) - 1))
 	  mul.s $f13, $f13, $f17 # f13 = f13 * f17  	# (‑abs(abs(x)-1))*abs(3-abs(x))
 	  mul.s $f15, $f15, $f16 # f15 = f15 * f16  	# ((abs(x)-1)*(3-abs(x)))
 		div.s $f13, $f13, $f15 # f13 = f13 / f15  	# (‑abs(abs(x)-1))*abs(3-abs(x)) / ((abs(x)-1)*(3-abs(x)))
 		sqrt.s $f13, $f13      # f13 = sqrt (f13) 	# sqrt((‑abs(abs(x)-1))*abs(3-abs(x)) / ((abs(x)-1)*(3-abs(x)))) 
 		mul.s $f13, $f13, $f24 # f13 = 2 * f13		 	# 2 * sqrt((‑abs(abs(x)-1))*abs(3-abs(x))/((abs(x)-1)*(3-abs(x)))) 
 
-		div.s $f15, $f0, $f26  # f15 = f0 / 7
-		mul.s $f15, $f15, $f15 # f15 = f15 * f15
-		sub.s $f15, $f22, $f15 # f15 = 1 - f15
-		sqrt.s $f15, $f15      # f15 = sqrt(f15)
+		div.s $f15, $f0, $f26  # f15 = f0 / 7				# x/7
+		mul.s $f15, $f15, $f15 # f15 = f15 * f15		# (x/7)^2
+		sub.s $f15, $f22, $f15 # f15 = 1 - f15			# 1 - (x/7)^2
+		sqrt.s $f15, $f15      # f15 = sqrt(f15)		# sqrt(1 - (x/7)^2)
 
-		sub.s $f16, $f0, $f30  # f16 = f0 - 0.5
-		abs.s $f16, $f16       # f16 = abs(f16)
-		add.s $f18, $f0, $f30  # f18 = f0 + 0.5
-		abs.s $f18, $f18       # f18 = abs(f18)
-		add.s $f16, $f16, $f18 # f16 = f16 + f18
-		mul.s $f16, $f29, $f16 # f16 = 0.97 * f16 
-		add.s $f16, $f28, $f16 # f16 = 5 + f16
-		sub.s $f17, $f0, $f31  # f17 = f0 - 0.75
-		abs.s $f17, $f17       # f17 = abs(f17)
-		add.s $f18, $f0, $f31  # f18 = f0 + 0.75
-		abs.s $f18, $f18       # f18 = abs(f18)
-		add.s $f17, $f17, $f18 #	f17 = f17 + f18
-		mul.s $f17, $f21, $f17 # f17 = 3 * f17
-		sub.s $f16, $f16, $f17 # f16 = f16 - f17		
-		add.s $f15, $f15, $f16 # f15 = f15 + f16 	# sqrt(1-(x/7)^2)+(5+0.97*(abs(x-0.5)+abs(x+0.5))-3 * (abs(x-0.75)+abs(x+0.75))) 
+		mul.s $f15, $f15, $f13 # f15 = f15 * f13    # 2 * sqrt((‑abs(abs(x)-1))*abs(3-abs(x))/((abs(x)-1)*(3-abs(x))))  * sqrt(1 - (x/7)^2)
 
-		abs.s $f16, $f0        # f16 = abs(f0)
-		sub.s $f16, $f22, $f16 # f16 = 1 - f16
-		add.s $f19, $f20, $f16 # f19 = f16
-		abs.s $f16, $f16       # f16 = abs(f16) 
-		div.s $f16, $f16, $f19 # f16 = f16 / f19
-		add.s $f16, $f22, $f16 # f16 = 1 + f16
-		mul.s $f15, $f15, $f16 # f15 = f15 * f16
+		sub.s $f16, $f0, $f30  # f16 = f0 - 0.5			# x - 0.5
+		abs.s $f16, $f16       # f16 = abs(f16)			# abs(x - 0.5)
+		add.s $f18, $f0, $f30  # f18 = f0 + 0.5			# x + 0.5
+		abs.s $f18, $f18       # f18 = abs(f18)			# abs(x + 0.5)
+		add.s $f16, $f16, $f18 # f16 = f16 + f18		# abs(x - 0.5) + abs(x + 0.5)
+		mul.s $f16, $f29, $f16 # f16 = 0.97 * f16 	# 0.97 * (abs(x - 0.5) + abs(x + 0.5))
+		add.s $f16, $f28, $f16 # f16 = 5 + f16			# 5 + 0.97 * (abs(x - 0.5) + abs(x + 0.5))
+		sub.s $f17, $f0, $f31  # f17 = f0 - 0.75		# x - 0.75
+		abs.s $f17, $f17       # f17 = abs(f17)			# abs(x - 0.75)
+		add.s $f18, $f0, $f31  # f18 = f0 + 0.75		# x + 0.75
+		abs.s $f18, $f18       # f18 = abs(f18)			# abs(x + 0.75)
+		add.s $f17, $f17, $f18 # f17 = f17 + f18		# abs(x - 0.75) + abs(x + 0.75)
+		mul.s $f17, $f21, $f17 # f17 = 3 * f17			# 3 * abs(x - 0.75) + abs(x + 0.75)
+		sub.s $f17, $f16, $f17 # f17 = f16 - f17		# 5 + 0.97 * (abs(x - 0.5) + abs(x + 0.5)) - 3 * abs(x - 0.75) + abs(x + 0.75)
 
-		abs.s $f14, $f0        # f14 = abs(f0)
-		sub.s $f14, $f14, $f21 # f14 = f14 - 3
-		add.s $f17, $f20, $f14 # f17 = f14 
-		abs.s $f14, $f14       # f14 = abs(f14)
-		div.s $f14, $f14, $f17 # f14 = f14 / f17
-		add.s $f14, $f22, $f14 # f14 = 1 + f14
-		mul.s $f14, $f14, $f15 # f14 = f14 * f15
+		abs.s $f16, $f0        # f16 = abs(f0)			# abs(x)
+		sub.s $f16, $f22, $f16 # f16 = 1 - f16			# 1 - abs(x)
+		add.s $f19, $f20, $f16 # f19 = f16					
+		abs.s $f16, $f16       # f16 = abs(f16) 		# abs(1 - abs(x))
+		div.s $f16, $f16, $f19 # f16 = f16 / f19		# abs(1 - abs(x))/(1 - abs(x)) 
+		add.s $f16, $f22, $f16 # f16 = 1 + f16			# 1 + abs(1 - abs(x))/(1 - abs(x)) 
+		mul.s $f17, $f17, $f16 # f17 = f16 * f17		# (1 + abs(1 - abs(x))/(1 - abs(x))) * (5 + 0.97 * (abs(x - 0.5) + abs(x + 0.5)) - 3 * abs(x - 0.75) + abs(x + 0.75))
 
-		mul.s $f12, $f13, $f14 # f12 = f13 * f14 
+		abs.s $f14, $f0        # f14 = abs(f0)			# abs(x)
+		sub.s $f14, $f14, $f21 # f14 = f14 - 3			# abs(x) - 3
+		add.s $f19, $f20, $f14 # f19 = f14 
+		abs.s $f14, $f14       # f14 = abs(f14)			# abs(abs(x) - 3)
+		div.s $f14, $f14, $f19 # f14 = f14 / f19		# abs(abs(x) - 3)/(abs(x) - 3)
+		add.s $f14, $f22, $f14 # f14 = 1 + f14			# 1 + abs(abs(x) - 3)/(abs(x) - 3)
+		mul.s $f15, $f15, $f14 # f15 = f15 * f14    # 2 * sqrt((‑abs(abs(x)-1))*abs(3-abs(x))/((abs(x)-1)*(3-abs(x))))  * sqrt(1 - (x/7)^2) * (1+​abs(​abs(​x)-​3)/​(abs(​x)-​3))
+
+		add.s $f12, $f15, $f17 # f12 = f15 + f17    # 2 * sqrt((‑abs(abs(x)-1))*abs(3-abs(x))/((abs(x)-1)*(3-abs(x))))  * sqrt(1 - (x/7)^2) * (1+​abs(​abs(​x)-​3)/​(abs(​x)-​3)) + (1 + abs(1 - abs(x))/(1 - abs(x))) * (5 + 0.97 * (abs(x - 0.5) + abs(x + 0.5)) - 3 * abs(x - 0.75) + abs(x + 0.75))
 
 		jr $ra
 
@@ -311,7 +313,7 @@
 		sw $ra, 0($sp)
 
 		DESENHA_CALCULA_PONTO:
-			jal FUNCAO_VERDE
+			jal FUNCAO_AZUL
 
 			# Verifica se o expoente é diferente de 255
 			mfc1 $s0, $f12
